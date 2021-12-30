8c36d7f07831316afa12ab0ebeaa06090f667877
wangdecheng <wangdecheng@xylink.com>
2021-12-30 17:40:29 +0800

    add

--- wdc/stragety001.py
+++ wdc/stragety001.py
@@ -6,13 +6,14 @@ import pandas as pd
 
 def strategy001(data, N=40, mu=1):
     MP = QA.MA((data.high + data.low + data.close) / 3, N)
+    MA5 = QA.MA(data.close, 5)
     concat = pd.concat(
         [abs(data.high - data.low), abs(data.high - data.close.shift(1)), abs(data.low - data.close.shift(1))], axis=1)
     TR = concat.max(axis=1)
     upBand = MP + mu * QA.MA(TR, N)
     dnBand = MP - mu * QA.MA(TR, N)
     FP = MP
-    return pd.DataFrame({'MP': MP, 'TR': TR,'MPDiff':MP.diff(), 'upBand': upBand, 'dnBand': dnBand})
+    return pd.DataFrame({'MP': MP, 'TR': TR,'MPDiff':MP.diff(), 'upBand': upBand, 'dnBand': dnBand,'MA5':MA5})
 
 def change_key(idx):
     new_idx = idx[0].strftime("%Y-%m-%d")
@@ -30,36 +31,39 @@ if __name__ == "__main__":
 
     lastprice = 0
     user = QA.QA_User(username='quantaxis',password='quantaxis') #用户
-    portfolio  = user.new_portfolio('strategy001')  #账户组合
-    acc = portfolio.new_account(account_cookie='acc001',init_cash=100000)
-    for idx, item in
-        .iterrows():
-        print(idx,item)
+    portfolio = user.new_portfolio('strategy001')  #账户组合
+    acc = portfolio.new_account(account_cookie='acc001',init_cash=1000000)
+    MPDiff = ind.MPDiff
+    MA5 = ind.MA5
+    for idx, item in data.iterrows():
+        #print(idx,item)
         #idx = change_key(idx)
-        if MPDiff.loc[idx] > 0 and item['close'] > ind.upBand.loc[idx]:
+        if item['close'] > MA5.loc[idx] and acc.hold_available.get(idx[1],0) ==0:
             print('buyOpen _ {}'.format(idx))
             acc.receive_simpledeal(
                 code=idx[1],
                 trade_price=item['close'],
                 trade_amount=1,
-                trade_towards=QA.ORDER_DIRECTION.BUY_OPEN,
+                trade_towards=QA.ORDER_DIRECTION.BUY,
                 trade_time=idx[0]
             )
-        if MPDiff.loc[idx] < 0 and item['close'] < ind.dnBand.loc[idx]:
+        if item['close'] < MA5.loc[idx] and acc.hold_available.get(idx[1],0) ==0:
             print('sellOPEN_ {}'.format(idx))
             acc.receive_simpledeal(
                 code=idx[1],
                 trade_price=item['close'],
                 trade_amount=1,
-                trade_towards=QA.ORDER_DIRECTION.SELL_OPEN,
+                trade_towards=QA.ORDER_DIRECTION.SELL,
                 trade_time=idx[0]
             )
         else:
-            print('close')
+            print('no andy option {}'.format(idx))
 
 
         lastprice = item['close']
     print('end')
 
     print(acc.history_table.tail(10))
+    performance = QA.QA_Performance(acc)
+    print(performance.pnl_fifo)
 
